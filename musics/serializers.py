f752ce1f28e98e3948d1f1d9a9fc30dcf7039198
sshyeri <sshyeri.k@gmail.com>
2019-04-23 05:20:42 +0000

    00_api server setting & music list

---
+++ musics/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Music
+class MusicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music
+        fields = ('id', 'title', 'artist')
